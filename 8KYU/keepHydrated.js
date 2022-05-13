71e6d5b59aef3805330438ad810f27a8047170f0
Onebestever <carla.h1248@gmail.com>
2022-05-12 22:35:32 -0400

    keepHydrated

---
+++ 8KYU/keepHydrated.js
@@ -0,0 +1,8 @@
+function litres(time) {
+  let litres = Math.floor(0.5*time)
+  return litres;
+}
+
+/*you can get liters by passing in time as an argumenr 
+mulitply time which would be hour * 0.5 how many liters are consumed within an hour
+then you would have to round down based on test */
