fae9db600771b58ee25a540212029231b77b1a53
Onebestever <carla.h1248@gmail.com>
2022-05-15 10:15:23 -0400

    addToArray using push method to add elemets to end of an array returingn new length of the array aka  mutating it

---
+++ 8KYU/addToArray.js
@@ -0,0 +1,13 @@
+/* add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:*/
+
+var websites = [];
+
+let newArray = websites.push("codewars")
+console.log(newArray)
+
+//or
+ websites.push("codewars")
+console.log(websites)
