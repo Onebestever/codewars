a6a2446f859fe8f0027d9cef1d77c6a22e64ec15
Onebestever <carla.h1248@gmail.com>
2022-05-22 23:23:48 -0400

    findTheMissingNumber.js

---
+++ 8KYU/findTheMissingNumber.js
@@ -0,0 +1,12 @@
+function missingNo(nums) {
+  for(let i = 0; i<=100; i++){
+    if(nums.includes(i) === true){ // the include methods lets you know if an argument is inlucded in an array or stroing 
+    }else{                           //if array includes whatever i is KEEP going & as soon as you hit a number numsArray doesnt include RETURN that number which is i
+      return i
+    }
+  }
+
+}
+  
+
+
