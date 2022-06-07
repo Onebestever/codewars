d488fc66c43666db8a5238b4053f45819c7088b7
Onebestever <carla.h1248@gmail.com>
2022-06-06 23:22:26 -0400

    reverseWords

---
+++ 8KYU/reverseWords.js
@@ -0,0 +1,9 @@
+/* Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.*/
+
+
+
+function reverseWords(str) {
+  let backwards= str.split('').reverse().join('').split(' ').reverse().join(' ')
+  return backwards
+
+}
