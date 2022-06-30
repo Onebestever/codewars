dfdf2990bf7a4b7061dc08a674b277d722e8c2f0
Onebestever <carla.h1248@gmail.com>
2022-06-30 00:03:09 -0400

    replaceDots

---
+++ 8KYU/FIXME-ReplaceAlldots.js
@@ -0,0 +1,5 @@
+var replaceDots = function(str) {
+// added the \ to escape special characters
+// added the g so that replace is run for all occurences in the string
+  return str.replace(/\./g, '-');
+}
